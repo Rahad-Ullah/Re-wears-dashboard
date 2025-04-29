dab694f67edbfb7b1f81174da418a9075b383fc2
Rahad Ullah <myemail.abc20@gmail.com>
2025-04-29 16:52:27 +0600

    Added support page

---
+++ src/types/support.ts
@@ -0,0 +1,13 @@
+import { ticketPriorities, ticketStatuses } from "@/constants/support";
+
+export type TicketPriority = (typeof ticketPriorities)[number];
+export type TicketStatus = (typeof ticketStatuses)[number];
+
+export interface ISupportTicket {
+  _id: number;
+  user: string;
+  subject: string;
+  priority: TicketPriority;
+  status: TicketStatus;
+  createdAt: string;
+}
