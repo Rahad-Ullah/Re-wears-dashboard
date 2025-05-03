3b4c0e279ab4b891734c05460c6071bf10ef1625
Rahad Ullah <myemail.abc20@gmail.com>
2025-05-03 11:48:21 +0600

    Added privacy policy in settings page

--- src/components/page/settings/TermsAndPolicy.tsx
+++ src/components/page/settings/TermsAndPolicy.tsx
@@ -1,6 +1,7 @@
 import { Tabs, TabsContent } from "@/components/ui/tabs";
 import TermsAndPolicyTabs from "./termsAndPolicy/TermsAndPolicyTabs";
 import TermsAndCondition from "./termsAndPolicy/TermsAndCondition";
+import PrivacyPolicy from "./termsAndPolicy/PrivacyPolicy";
 
 const TermsAndPolicy = async ({ searchParams }) => {
   const { nestedTab } = await searchParams;
@@ -11,6 +12,9 @@ const TermsAndPolicy = async ({ searchParams }) => {
 
       <TabsContent value="terms-and-condition">
         <TermsAndCondition />
+      </TabsContent>
+      <TabsContent value="privacy-policy">
+        <PrivacyPolicy />
       </TabsContent>
     </Tabs>
   );
