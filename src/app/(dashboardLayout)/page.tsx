ad65bc388aa0ce4f79483a06b18cddd7cfcea02c
Rahad Ullah <myemail.abc20@gmail.com>
2025-05-03 14:25:50 +0600

    Added statistics card

---
+++ src/app/(dashboardLayout)/page.tsx
@@ -0,0 +1,12 @@
+import StatisticsCards from "@/components/page/analytics/StatisticsCards";
+import { Card } from "@/components/ui/card";
+
+const AnalyticsPage = () => {
+  return (
+    <Card className="h-full p-5">
+      <StatisticsCards />
+    </Card>
+  );
+};
+
+export default AnalyticsPage;
