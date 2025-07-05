8d79d4f30ee9609cb97a8bd4b1627dfc00fde68d
jewelrana3 <juyelrana7752@gmail.com>
2025-07-05 20:07:49 +0600

    edit issue image is required

--- src/components/page/categories/forms/EditSubCategoryForm.tsx
+++ src/components/page/categories/forms/EditSubCategoryForm.tsx
@@ -52,6 +52,8 @@ export default function EditSubCategoryForm({ item }) {
         body: formData,
       });
 
+      console.log(res);
+
       if (res.success) {
         toast.success(res.message || "Edit successfully", { id: "sub" });
         await revalidateTags(["sub-category"]);
