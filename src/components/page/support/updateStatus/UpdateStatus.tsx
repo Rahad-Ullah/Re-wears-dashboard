ea037b09c4423726640246ac3731217b02e1e5fe
jewelrana3 <juyelrana7752@gmail.com>
2025-08-25 11:56:30 +0600

    support page add email send admin to user

--- src/components/page/support/updateStatus/UpdateStatus.tsx
+++ src/components/page/support/updateStatus/UpdateStatus.tsx
@@ -2,6 +2,7 @@
 
 import Modal from "@/components/modals/Modal";
 import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
 import { revalidateTags } from "@/helpers/revalidateHelper";
 import { myFetch } from "@/utils/myFetch";
 import { Eye } from "lucide-react";
@@ -10,6 +11,42 @@ import toast from "react-hot-toast";
 
 export default function UpdateStatus({ item }) {
   const [open, setOpen] = useState(false);
+
+  const handleMessageSend = async (
+    e: React.FormEvent<HTMLFormElement>,
+    email: string
+  ) => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    const formData = new FormData(form);
+
+    const message = formData.get("message")?.toString().trim();
+
+    const data = {
+      email,
+      message,
+    };
+
+    const res = await myFetch(`/supports/replay`, {
+      method: "POST",
+      body: data,
+    });
+
+    try {
+      if (res?.success) {
+        // toast.success("Send message successfully");
+        await revalidateTags(["message"]);
+        setOpen(false);
+        form.reset();
+      } else {
+        toast.error(res?.message || "failed message try again");
+      }
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (error) {
+      toast.error(res?.data?.message);
+    }
+  };
+
   const handleStatusUpdate = async (status: string) => {
     toast.loading("Procesing.....", { id: "status" });
 
@@ -59,18 +96,43 @@ export default function UpdateStatus({ item }) {
           <p className="font-medium">
             <strong>Subject:</strong> {item?.subject}
           </p>
-          <div className="flex items-center gap-4 justify-end mt-2">
-            {item?.status === "OPEN" && (
-              <Button onClick={() => handleStatusUpdate("PENDING")}>
-                Mark as pending
-              </Button>
-            )}
-            {(item?.status === "PENDING" || item?.status === "OPEN") && (
-              <Button onClick={() => handleStatusUpdate("RESOLVED")}>
-                Mark as resolved
-              </Button>
-            )}
-          </div>
+
+          {item?.status !== "RESOLVED" ? (
+            <form
+              onSubmit={async (e) => {
+                e.preventDefault();
+                await handleMessageSend(e, item?.email);
+                await handleStatusUpdate("PENDING");
+              }}
+              className="grid gap-3"
+            >
+              <>
+                <h1 className="font-semibold">Send Message : </h1>
+                <Textarea
+                  name="message"
+                  rows={4}
+                  placeholder="Write here..."
+                  required
+                />
+              </>
+
+              <div className="flex items-center gap-4 justify-end mt-2">
+                <Button type="submit" className="px-6">
+                  Send
+                </Button>
+
+                {
+                  <Button onClick={() => handleStatusUpdate("RESOLVED")}>
+                    Mark as resolved
+                  </Button>
+                }
+              </div>
+            </form>
+          ) : (
+            <p>
+              Status: <span className="text-green-600">Resolved</span>
+            </p>
+          )}
         </div>
       </Modal>
     </div>
